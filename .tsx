e1c9ea6daa445794dc9683e923869ab308cd8c7f
GWalk123 <gregorywalker760@gmail.com>
2025-09-04 07:14:30 -0400

    Integrate Customize Your Experience feature into HomePage

---
+++ .tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CustomizeExperience from '../components/CustomizeExperience';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <CustomizeExperience />
+    </div>
+  );
+};
+
+export default HomePage;
